dc85494c9c5e644bff92d85865e6d32464abf242
Talgat Tokayev <ttokayev7@gmail.com>
2024-08-05 18:37:35 +0500

    hero part1

--- DiveSea/src/index.js
+++ DiveSea/src/index.js
@@ -1,23 +1,4 @@
 import './index.html';
 import './index.scss';
 
-const nav = document.querySelector('.nav')
-const hamburger = document.querySelector('.hamburger')
-
-const body = document.body;
-
-if (nav && hamburger) {
-	hamburger.addEventListener('click', e => {
-		nav.classList.toggle('_active')
-		hamburger.classList.toggle('_active')
-		body.classList.toggle('_lock')
-	})
-
-	nav.addEventListener('click', e => {
-		if (e.target.classList.contains('hamburger')) {
-			nav.classList.remove('_active')
-			hamburger.classList.remove('_active')
-			body.classList.remove('_lock')
-		}
-	})
-}
+import './js/burger';
